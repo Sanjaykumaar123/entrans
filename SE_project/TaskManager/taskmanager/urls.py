73ef988160303566866af67b80ab1cbb2c425c7e
Sanjaykumaar123 <gksanjaykumaar.cse2024@citchennai.net>
2025-11-13 16:27:37 +0530

    Update urls.py

--- SE_project/TaskManager/taskmanager/urls.py
+++ SE_project/TaskManager/taskmanager/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import home
+from todo.views import home, signup_view   # ⬅ add signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+
+    # API routes
     path('api/', include('todo.urls')),
+
+    # Auth (login, logout)
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # Signup route
+    path('accounts/signup/', signup_view, name='signup'),
 ]
